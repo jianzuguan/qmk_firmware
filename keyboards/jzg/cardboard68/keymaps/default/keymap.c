d2fcd4c3a355b5dde7791edd90d06caea880d63a
Jianzu Guan <jianzuguan@luminalearning.com>
2022-02-18 20:35:49 +0000

    swap mouse toggle key

--- keyboards/jzg/cardboard68/keymaps/default/keymap.c
+++ keyboards/jzg/cardboard68/keymaps/default/keymap.c
@@ -53,10 +53,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_DEFAULT] = LAYOUT(
           KC_F12, KC_1, KC_2,    KC_3,    KC_4,    KC_5,   KC_F5,                       KC_PSCR,    KC_6,     KC_7,    KC_8,   KC_9,    KC_0,  KC_DEL,
-          KC_TAB, KC_Q, KC_W,    KC_E,    KC_R,    KC_T,  KC_DEL,                      TT_MOUSE,    KC_Y,     KC_U,    KC_I,   KC_O,    KC_P, KC_BSPC, 
-          KC_ESC, KC_A, KC_S,    KC_D,    KC_F,    KC_G,  KC_ENT,                          BASE,    KC_H,     KC_J,    KC_K,   KC_L, KC_SCLN,  KC_ENT, 
-         KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,    KC_B, KC_CAPS, KC_LGUI,    KC_RGUI,      NUM,    KC_N,     KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
-                                 BASE, KC_LALT, KC_LCTL,  KC_SPC,     SYM,        NAV,   KC_SPC, KC_RCTL, TG_MOUSE, KC_RALT
+          KC_TAB, KC_Q, KC_W,    KC_E,    KC_R,    KC_T,  KC_DEL,                      TG_MOUSE,    KC_Y,     KC_U,    KC_I,   KC_O,    KC_P, KC_BSPC,
+          KC_ESC, KC_A, KC_S,    KC_D,    KC_F,    KC_G,  KC_ENT,                          BASE,    KC_H,     KC_J,    KC_K,   KC_L, KC_SCLN,  KC_ENT,
+         KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,    KC_B, KC_CAPS, KC_LGUI,    KC_RGUI,      NUM,    KC_N,     KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                                 BASE, KC_LALT, KC_LCTL,  KC_SPC,     SYM,        NAV,   KC_SPC, KC_RCTL, TT_MOUSE, KC_RALT
     ),
 
     [_SYM] = LAYOUT(
