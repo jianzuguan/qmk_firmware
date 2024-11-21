09d144d8043237cfa42514907998d4f5149bfc2a
Jianzu Guan <git@jzg.fastmail.com>
2024-11-21 21:43:13 +0000

    add number and navigation layers to keyboard layout

--- keyboards/jzg/cardboard68/keymaps/default/keymap.c
+++ keyboards/jzg/cardboard68/keymaps/default/keymap.c
@@ -18,11 +18,13 @@
 enum layers {
     _WIN_DFLT = 0,
     _WIN_SYM,
+    _WIN_NUM,
+    _WIN_NAV,
     _MAC_DFLT,
     _MAC_SYM,
-    _NAV,
+    _MAC_NUM,
+    _MAC_NAV,
     _MOUSE,
-    _NUM,
     _FPS
 };
 
@@ -33,8 +35,10 @@ enum layers {
 
 #define WIN_SYM  TT(_WIN_SYM)
 #define MAC_SYM  TT(_MAC_SYM)
-#define NUM      TT(_NUM)
-#define NAV      TT(_NAV)
+#define WIN_NUM  TT(_WIN_NUM)
+#define MAC_NUM  TT(_MAC_NUM)
+#define WIN_NAV  TT(_WIN_NAV)
+#define MAC_NAV  TT(_MAC_NAV)
 #define TT_MOUSE TT(_MOUSE)
 #define TG_MOUSE TG(_MOUSE)
 
@@ -66,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,   KC_DEL,                      WIN_DFLT,    KC_Y,     KC_U,    KC_I,   KC_O,    KC_P, KC_BSPC,
           KC_ESC,  KC_A,  KC_S,    KC_D,    KC_F,    KC_G,   KC_ENT,                      TG_MOUSE,    KC_H,     KC_J,    KC_K,   KC_L, KC_SCLN,  KC_ENT,
          KC_LSFT,  KC_Z,  KC_X,    KC_C,    KC_V,    KC_B, TG_MOUSE, KC_LGUI,    KC_RGUI,  KC_CAPS,    KC_N,     KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                               WIN_DFLT, KC_LALT, KC_LCTL,   KC_SPC,     NUM,        NAV,  WIN_SYM,  KC_SPC,  KC_RCTL, KC_RALT
+                               WIN_DFLT, KC_LALT, KC_LCTL,   KC_SPC, WIN_NUM,    WIN_NAV,  WIN_SYM,  KC_SPC,  KC_RCTL, KC_RALT
     ),
 
     [_WIN_SYM] = LAYOUT(
@@ -77,12 +81,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
     ),
 
+    [_WIN_NUM] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______, _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,                      _______, KC_PSLS,    KC_7,    KC_8,    KC_9, KC_PMNS, _______,
+        _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX, _______,                      _______, KC_PAST,    KC_4,    KC_5,    KC_6, KC_PPLS, _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,    _______, _______,    KC_0,    KC_1,    KC_2,    KC_3, KC_PDOT, _______,
+                                   _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
+    ),
+
+    [_WIN_NAV] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______, _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, _______,                      _______, KC_PGUP, KC_HOME,   KC_UP,  KC_END, XXXXXXX, _______, 
+        _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NLCK, _______,                      _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, _______, 
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,    _______, _______,  KC_INS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, 
+                                   _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
+    ),
+
     [_MAC_DFLT] = LAYOUT(
           KC_F12, KC_F1, KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_BSPC,                       KC_PSCR,   KC_F6,    KC_F7,   KC_F8,  KC_F9,  KC_F10,  KC_F11,
           KC_TAB,  KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,   KC_DEL,                      MAC_DFLT,    KC_Y,     KC_U,    KC_I,   KC_O,    KC_P, KC_BSPC,
           KC_ESC,  KC_A,  KC_S,    KC_D,    KC_F,    KC_G,   KC_ENT,                      TG_MOUSE,    KC_H,     KC_J,    KC_K,   KC_L, KC_SCLN,  KC_ENT,
          KC_LSFT,  KC_Z,  KC_X,    KC_C,    KC_V,    KC_B, TG_MOUSE, KC_LCTL,    KC_RCTL,  KC_CAPS,    KC_N,     KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                               MAC_DFLT, KC_LCMD, KC_LCMD,   KC_SPC,     NUM,        NAV,  MAC_SYM,  KC_SPC,  KC_RCMD, KC_ROPT
+                               MAC_DFLT, KC_LOPT, KC_LCMD,   KC_SPC, MAC_NUM,    MAC_NAV,  MAC_SYM,  KC_SPC,  KC_RCMD, KC_ROPT
     ),
 
     [_MAC_SYM] = LAYOUT(
@@ -93,27 +113,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
     ),
 
-    [_NUM] = LAYOUT(
+    [_MAC_NUM] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______, _______,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,                      _______, KC_PSLS,    KC_7,    KC_8,    KC_9, KC_PMNS, _______,
-        _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX, _______,                      _______, KC_PAST,    KC_4,    KC_5,    KC_6, KC_PPLS, _______,
+        _______, KC_LCTL, KC_LOPT, KC_LSFT, KC_LCMD, XXXXXXX, _______,                      _______, KC_PAST,    KC_4,    KC_5,    KC_6, KC_PPLS, _______,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,    _______, _______,    KC_0,    KC_1,    KC_2,    KC_3, KC_PDOT, _______,
                                    _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
     ),
 
-    [_NAV] = LAYOUT(
+    [_MAC_NAV] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______, _______,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, _______,                      _______, KC_PGUP, KC_HOME,   KC_UP,  KC_END, XXXXXXX, _______, 
-        _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_NLCK, _______,                      _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, _______, 
+        _______, KC_LCTL, KC_LOPT, KC_LSFT, KC_LCMD, KC_NLCK, _______,                      _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, _______, 
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,    _______, _______,  KC_INS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, 
                                    _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
     ),
 
     [_MOUSE] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______,                      _______,  _______, _______, _______, _______, _______, _______,
-        _______, KC_VOLU, KC_WH_L, KC_MS_U, KC_WH_R, KC_WH_U, _______,                      _______, MAC_DFLT, KC_VOLU, KC_PAUS, KC_MPRV, XXXXXXX, _______,
-        _______, KC_VOLD, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______,                      _______, WIN_DFLT, KC_VOLD, KC_MPLY, KC_MNXT, XXXXXXX, _______,
-        _______, KC_MUTE, KC_WBAK, KC_WREF, KC_WFWD, XXXXXXX, _______, _______,    _______, _______,      FPS, KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        _______, KC_VOLU, KC_WH_L, KC_MS_U, KC_WH_R, KC_WH_U, _______,                      _______,      FPS, KC_VOLU, KC_PAUS, KC_MPRV, XXXXXXX, _______,
+        _______, KC_VOLD, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______,                      _______, MAC_DFLT, KC_VOLD, KC_MPLY, KC_MNXT, XXXXXXX, _______,
+        _______, KC_MUTE, KC_WBAK, KC_WREF, KC_WFWD, XXXXXXX, _______, _______,    _______, _______, WIN_DFLT, KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                                    _______, _______, KC_BTN2, KC_BTN1, _______,    _______, _______,  _______, _______, _______
     ),
 
